b9b3e642e409dd1bec88386456abea9c6959d865
Alejandro Gutiérrez <alejandro_gutierrez.i@outlook.cl>
2020-02-05 14:46:09 -0300

    crear peticion get con Rutas de NodeJs

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+
+app.use(router);
+
+router.get('/', (req, res)=>{
+    res.send('hola desde get');
+})
+
+app.listen(3000);
+console.log('escuchando desde 3000');
