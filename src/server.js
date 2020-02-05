356a6e657bf57c8912aaa1c7d9af4f89be7034f9
Alejandro Gutiérrez <alejandro_gutierrez.i@outlook.cl>
2020-02-05 11:18:44 -0300

    pruebas con rutas para recivir y enviar peticiones analizando sus headers y body

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const router = express.Router();
 const app = express();
+const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
 
-app.use('/', (req, res) => {
-    res.send("hola");
+router.get('/', (req, res) => {
+    res.send(req.headers);
+
+    res.send("funciona get")
 })
 
-app.listen(3000);
-console.log("app escuchando en 3000");
+router.post('/message', (req, res) => {
+    console.log(req.body);
+    console.log(req.query);
+    res.send("dale")
+
+})
+
+app.listen(port || process.env.port);
+console.log(`escuchando desde ${port || process.env.port}`);
