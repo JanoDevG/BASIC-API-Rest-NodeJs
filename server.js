43f77e4a9c74905575dab74ef3e6813f1e45259c
Alejandro Gutiérrez <alejandro_gutierrez.i@outlook.cl>
2020-02-05 15:08:12 -0300

    prueba de envío de peticiones

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require('express');
 const router = express.Router();
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(router);
 
 router.get('/message', (req, res)=>{
     res.send('hola desde get');
 })
 
-router.post('/message', (req, res)=>{
-    res.send('hola desde post');
+router.delete('/message', (req, res)=>{
+    console.log(req.query);
+    console.log(req.body);
+    res.send('eliminado correctamente el mensaje: '+ req.body.text);
 })
 
 app.listen(3000);
