618b6eb60c547f82e936f263430bc1b4e83965c0
Alejandro Gutiérrez <alejandro_gutierrez.i@outlook.cl>
2020-02-10 18:25:09 -0300

    DELETE

--- components/message/controller.js
+++ components/message/controller.js
@@ -38,8 +38,25 @@ function updateMessage(id, message) {
         resolve(result);
     })
 }
+
+function deleteMessage(id) {
+    return new Promise((resolve, reject) => {
+        if (!id) {
+            reject('id inválido');
+        }
+        store.remove(id)
+            .then(() => {
+                resolve();
+            })
+            .catch(e => {
+                reject(e);
+            });
+    });
+}
+
 module.exports = {
     addMessage,
     getMessages,
-    updateMessage
+    updateMessage,
+    deleteMessage
 };
--- components/message/network.js
+++ components/message/network.js
@@ -30,10 +30,21 @@ router.patch('/:id', (req, res) => {  //nos llega el  id
   console.log(`se está consultando el id: ${req.params.id}`);
   controller.updateMessage(req.params.id, req.body.message)  //se envían los datos al método updateMessage del controlador
     .then((data) => {
-      response.success(req, res, data, 200)
+      response.success(req, res, data, 200);
     })
     .catch(e => {
-      response.error(req, res, 'error inesperado', 500, e)
+      response.error(req, res, 'error inesperado', 500, e);
     })
 })
+
+router.delete('/:id', (req, res) => {
+  controller.deleteMessage(req.params.id)
+    .then(() => {
+      response.success(req, res, `usuario: ${req.params.id} fue eliminado`, 200);
+    })
+    .catch(e => {
+      response.error(req, res, 'error interno', 500, e);
+    })
+})
+
 module.exports = router;
